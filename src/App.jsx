f61d7f6c286c05dc77ef7c89d9ad532121cb78f1
Payel Acharyya <121756390+Payel-warrior@users.noreply.github.com>
2025-07-28 19:20:42 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header.jsx';
+import About from './components/About.jsx';
 
 function App() {
   return (
     <div>
       <Header/>
+      <About/>
     </div>
   )
 }
