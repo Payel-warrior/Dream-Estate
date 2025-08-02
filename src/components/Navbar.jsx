d622b850725842472fbfd4d05eef59446f4b9224
Payel Acharyya <121756390+Payel-warrior@users.noreply.github.com>
2025-08-02 20:15:12 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from 'react';
 import { assets } from '../assets/assets';
+import { Link  } from "react-router-dom";
 
 
 
@@ -17,17 +18,17 @@ function Navbar() {
 },[displaySidebar])
 
   return (
-    <div className='absolute top-0 left-0 w-full z-10'>
+    <div className='fixed top-0 left-0 w-full h-min z-10'style={{ backgroundColor: 'oklch(0.17 0 0 / 0.52)' }}>
         <div className='container flex mx-auto justify-between items-center py-4 px-6 md:px-10 lg:px-20 '>
             <div className='justify-center items-center'>
             <h2 className='text-emerald-600 font-semibold text-xl ml-2.5'>Dream Estate</h2>
             <p className='text-white text-sm mt-0'> A Real Estate Company</p>
             </div>
             <ul className='hidden md:flex gap-7 text-white font-semibold text-xl'>
-                <a href="#Header">Header</a>
-                <a href="#About">About</a>
-                <a href="#Properties">Properties</a>
-                <a href="#Reviews">Feedbacks</a>
+                <li><Link to="/">Home</Link></li>
+                <li><a href="#About">About</a></li>
+                <li><Link to="Properties">Properties</Link></li>
+                <li><a href="#Reviews">Feedbacks</a></li>
             </ul>
             <button className='hidden md:block bg-emerald-600 px-6 py-1.5 rounded-md text-white font-semibold cursor-pointer hover:px-7 hover:py-2 '>Sign Up</button>
            <img onClick={()=> setDisplaySidebar(true)}src={assets.sidebar4} alt="" className='md:hidden w-15' /> 
@@ -38,11 +39,11 @@ function Navbar() {
              <img onClick={()=>setDisplaySidebar(false)}src={assets.sidebar3} alt="" className='w-15 cursor-pointer'/>
             </div>
             <ul className='flex flex-col items-start gap-2 mt-5 px-5 text-lg font-medium'>
-                <a onClick={()=>setDisplaySidebar(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Header</a>
-                <a onClick={()=>setDisplaySidebar(false)} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
-                <a onClick={()=>setDisplaySidebar(false)} href="#Properties" className='px-4 py-2 rounded-full inline-block'>Properties</a>
-                <a onClick={()=>setDisplaySidebar(false)}
-                href="#Reviews" className='px-4 py-2 rounded-full inline-block'>Feedbacks</a>
+                <li><Link to="/" onClick={() => setDisplaySidebar(false)}className='px-4 py-2 rounded-full inline-block'> Home </Link></li>
+                <li><a onClick={()=>setDisplaySidebar(false)} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a></li>
+                <li> <Link to="Properties" onClick={() => setDisplaySidebar(false)}className='px-4 py-2 rounded-full inline-block'> Properties </Link> </li>
+              <li><a onClick={()=>setDisplaySidebar(false)}
+                href="#Reviews" className='px-4 py-2 rounded-full inline-block'>Feedbacks</a></li>
             </ul>
         </div>
 
@@ -51,3 +52,5 @@ function Navbar() {
 }
 
 export default Navbar
+
+
