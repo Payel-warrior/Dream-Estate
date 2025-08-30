995124f3e8fcdafed9179c17dd85e25dbd3c2025
Payel Acharyya <121756390+Payel-warrior@users.noreply.github.com>
2025-08-30 19:22:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,68 @@ import Header from './components/Header.jsx';
 import About from './components/About.jsx';
 import Properties from './components/Properties.jsx';
 import Reviews from './components/Reviews.jsx';
+import SignUp from "./components/SignUp.jsx";
 import Contact from './components/Contact.jsx'
 import Layout from './Layout.jsx';
-import {Routes, Route } from "react-router-dom";
+import {Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import PageWrapper from "./components/pageWrapper.jsx";
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Home route with header + about shown */}
-        <Route index element={
-          <>
-            <Header />
-            <About />
-            <Reviews />
-          </>
-        } />
-        <Route path="Properties" element={<Properties />} />
-        <Route path="Contact" element={<Contact />} />
-      </Route>
-    </Routes>
-  )
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Layout />}>
+          {/* Home: smooth fade */}
+          <Route
+            index
+            element={
+              <PageWrapper variant="fade">
+                <Header />
+                <About />
+                <Reviews />
+              </PageWrapper>
+            }
+          />
+
+          {/* Properties: slide up */}
+          <Route
+            path="Properties"
+            element={
+              <PageWrapper variant="slideUp">
+                <Properties />
+              </PageWrapper>
+            }
+          />
+
+          {/* Contact: zoom in */}
+          <Route
+            path="Contact"
+            element={
+              <PageWrapper variant="zoom">
+                <Contact />
+              </PageWrapper>
+            }
+          />
+
+          {/* SignUp: slide from right */}
+          <Route
+            path="SignUp"
+            element={
+              <PageWrapper variant="slideRight">
+                <SignUp />
+              </PageWrapper>
+            }
+          />
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
 }
 
-export default App
+export default AnimatedRoutes;
 
 
 
